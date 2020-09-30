69abf0f042849a9f8aa7c2495a95ddce2a117500
Ekaterinafieryglow <71172000+Ekaterinafieryglow@users.noreply.github.com>
2020-09-30 12:38:49 +0300

    Проект practica1
    Совершила загрузку проекта

---
+++ practice1/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "RUS");
+	char str[80] = { 0 };
+
+	string command;
+	int amount;
+
+	cout << "Cообщение вводится БЕЗ пробелов на АНГЛИЙСКОМ языке!" << endl;
+	cout << "Введите сообщение для кодировки: ";
+	cin >> str;
+	cout << "Введите сдвиг: ";
+	cin >> amount;
+
+	do
+	{
+		cout << "Возможные команды: encode, decode. Ваша команда:  ";
+		cin >> command;
+		if (command == "decode")
+		{
+			for (int i = 0; i < strlen(str); i++)
+			{
+				{
+					str[i] -= amount;
+				}
+			}
+			cout << "Зашифрованное сообщение:  " << str << endl;
+		}
+		else
+		{
+			for (int i = 0; i < strlen(str); i++)
+			{
+				str[i] += amount;
+			}
+			cout << "Расшифрованное сообщение:  " << str << endl;
+		}
+	} while ((command != "encode") and (command != "decode"));
+
+	system("pause");
+	return 0;
+}

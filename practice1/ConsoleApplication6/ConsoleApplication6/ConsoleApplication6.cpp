514eee616545949112e65d3db6d17d2fb9e7d37f
Ekaterinafieryglow <71172000+Ekaterinafieryglow@users.noreply.github.com>
2020-11-01 15:46:03 +0300

    Лабораторная работа 6(1)
    Доработка диалогового режима с пользователем и отработка ошибок ввода: проверка на числовой формат сдвига, на правильность ввода команды, на формат символов при вводе сообщения для шифровки/расшифровки.

--- practice1/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
+++ practice1/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,44 +1,74 @@
-﻿#include <iostream>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
 using namespace std;
 
 int main()
 {
-	setlocale(0, "RUS");
-	char str[80] = { 0 };
+	setlocale(0, "rus");
+	char str[80], result[80] = { 0 };
 
-	string command;
-	int amount;
+	string command, amount;
+	char cipher;
+	int bias;
+	bool key;
 
 	cout << "Cообщение вводится БЕЗ пробелов на АНГЛИЙСКОМ языке!" << endl;
-	cout << "Введите сообщение для кодировки: ";
+	cout << "Введите сообщение для кодировки: ";    
 	cin >> str;
-	cout << "Введите сдвиг: ";
-	cin >> amount;
 
 	do
 	{
-		cout << "Возможные команды: encode, decode. Ваша команда:  ";
-		cin >> command;
-		if (command == "decode")
+		key = true;
+		cout << "Величина сдвига: ";  
+		cin >> amount;
+		for (int i = 0; i < amount.length(); i++)  
 		{
-			for (int i = 0; i < strlen(str); i++)
+			if ((!isdigit(amount[i])) and (amount[i] != '-'))
 			{
-				{
-					str[i] -= amount;
-				}
+				key = false;
+				cout << "Вы ввели не число! Попробуйте ввести еще раз!" << endl;
+				break;
 			}
-			cout << "Зашифрованное сообщение:  " << str << endl;
 		}
-		else
+	} while (!key);
+	bias = atoi(amount.c_str());  
+
+	do
+	{
+		cout << "Возможные команды: encode, decode. Ваша команда:  ";   /
+		cin >> command;
+		if (command == "decode")  
 		{
-			for (int i = 0; i < strlen(str); i++)
-			{
-				str[i] += amount;
-			}
-			cout << "Расшифрованное сообщение:  " << str << endl;
+			bias *= -1;
+		}
+		else if (command != "encode") 
+		{
+			cout << "Команда введена не верно! Попробуйте ввести еще раз!" << endl;
 		}
 	} while ((command != "encode") and (command != "decode"));
 
+	for (int i = 0; i < strlen(str); i++)  
+	{
+		if ((str[i] > 96) and (str[i] < 123)) 
+		{
+			cipher = str[i] += bias;
+			cipher = char(97 + ((cipher - 97) % 26));
+			result[i] = cipher;
+		}
+		else if ((str[i] > 64) and (str[i] < 91)) 
+		{
+			cipher = str[i] += bias;
+			cipher = char(65 + ((cipher - 65) % 26));
+			result[i] = cipher;
+		}
+		else
+		{
+			result[i] = str[i];
+		}
+	}
+	cout << "Зашифрованное сообщение:  " << result << endl; 
 	system("pause");
 	return 0;
 }

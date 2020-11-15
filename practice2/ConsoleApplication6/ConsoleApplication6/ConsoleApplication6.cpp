a44757691f7c3d7b047e199b600e2b720d8ca90f
Ekaterinafieryglow <71172000+Ekaterinafieryglow@users.noreply.github.com>
2020-11-16 01:11:50 +0300

    Лабораторная работа 9-10
    Проект с программой, реализующей метод анализа иерархий Томаса Саати для одного уровня.

---
+++ practice2/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    float  rows(NULL);
+    cout << "Введите количество критериев: ";
+    cin >> rows;
+
+    float **arr = new float* [rows];    
+    float *arrSum = new float [rows];   
+   
+    for (int i = 0; i < rows; i++)  
+    {
+        arr[i] = new float[rows];
+    }
+
+    for (int i = 0; i < rows; i++)    
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            cout << i + 1 << "-я строка ";
+            cout << j + 1 << "-й столбец: ";
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        arrSum[i] = 0;
+        for (int j = 0; j < rows; j++)
+        {
+            cout << arr[i][j] << "\t";
+            arrSum[i] += arr[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "\nСумма по строкам!\n";
+    float arrSumO(NULL);
+    for (int i = 0; i < rows; i++)
+    {
+        cout << i + 1 << "-й столбец: " << arrSum[i] << endl;
+        arrSumO = arrSumO + arrSum[i];
+    }
+    cout << "\nОбщая сумма строк!\n";
+    cout << arrSumO << endl;
+
+    float SumO;
+    SumO = (arrSumO / arrSumO);
+    cout << SumO << endl;
+
+    for (int i = 0; i < rows; i++)
+    {
+        cout << i + 1 << "-й столбец: " << round(arrSum[i] / arrSumO * 100) / 100 << endl;
+    }
+
+
+
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] arr[i];
+    }
+
+    delete[] arr;
+    delete[] arrSum;
+}
